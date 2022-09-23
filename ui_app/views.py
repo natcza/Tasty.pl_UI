14fa301fddf787738d6d079ff8a352da7212a652
jz-cl <janusz.zychowicz@gmail.com>
2022-09-23 23:00:30 +0200

    err 500

--- ui_app/views.py
+++ ui_app/views.py
@@ -6,19 +6,26 @@ from django.http import HttpRequest, HttpResponse, Http404
 from json import loads
 from Dashboard.settings import SERVER_DEVELOPMENT
 
+
 class RestaurantsView(View):
     def get(self, request):
-        print('blablabla', request.data)
-        url_restaurant = requests.get(SERVER_DEVELOPMENT + '/restaurants/')
         # print('blablabla', request.data)
-        arr1 = []
-        for i in loads(url_restaurant.text):
-            arr1.append((i['name'], i['city']))
-        print(arr1[0][0])
-        ctx = {
-            'restaurants': arr1,
-        }
-        return render(request, 'base.html', ctx)
+        url_restaurant = requests.get(SERVER_DEVELOPMENT + '/restaurants/')
+        code = url_restaurant.status_code
+        if code == 200:
+            print(url_restaurant.status_code)
+            # print('blablabla', request.data)
+            arr1 = []
+            for i in loads(url_restaurant.text):
+                arr1.append((i['name'], i['city']))
+            print(arr1[0][0])
+            ctx = {
+                'restaurants': arr1,
+            }
+            return render(request, 'base.html', ctx)
+        elif code == 404:
+            raise Http404("No MyModel matches the given query.")
+
 
 class RestaurantDetailView(View):
     def get(self, request, restaurant_id):
