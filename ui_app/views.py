ea0ae15692c7e2bc89ce3c543cd3dc240d2f80fb
natcza <natalia.nc.czajkowska@gmail.com>
2022-12-06 22:51:33 +0100

    update of list restaurants

--- ui_app/views.py
+++ ui_app/views.py
@@ -9,18 +9,17 @@ from Dashboard.settings import SERVER_DEVELOPMENT
 class RestaurantsView(View):
     def get(self, request):
         restaurants = requests.get(SERVER_DEVELOPMENT + '/restaurants/')
-        arr1 = []
-        res_1 = loads(restaurants.text)
-        print(res_1["results"][0])
-        for i in res_1["results"]:
-            arr1.append((i['name'], i['city'], i["street"], i['house_number'], i['phone_number']))
-        print(arr1[0][0])
+        restaurant_list = []
+        load_data_json = loads(restaurants.text)
+        print(load_data_json["results"][0])
+        for i in load_data_json["results"]:
+            restaurant_list.append((i['name'], i['city'], i["street"], i['house_number'], i['phone_number']))
         ctx = {
-            'restaurants': arr1,
+            'restaurants': restaurant_list,
         }
         return render(request, 'base.html', ctx)
 
 class RestaurantDetailView(View):
-    def get(self, request, restaurant_id):
+    def get(self, request, pk):
         pass
         # url_restaurant = requests.get('http://127.0.0.1:8001/restaurants/')
